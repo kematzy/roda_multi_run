0f3eb6b88ddd2f42569f6e8ed097815bcc04383e
Kematzy <kematzy@gmail.com>
2015-05-04 14:44:59 +0800

    App2: updates
    -- Setting full path to sub dir
    -- added opts[:add_script_name] = true
    -- added assets with SCSS rendering

--- apps/app2/current/app2.rb
+++ apps/app2/current/app2.rb
@@ -1,8 +1,11 @@
 class App2 < Roda
-  opts[:root] = "sub"
+  opts[:root] = File.expand_path('sub', File.dirname(__FILE__))
+  opts[:add_script_name] = true
+  
   plugin :render
-  plugin :static, ["/images"]
-  plugin :assets, css: 'app.css'
+  # plugin :static, [File.expand_path('/images', File.dirname(__FILE__))]
+  # plugin :static, ['/images']
+  plugin :assets, css: 'app.scss', path: File.expand_path('assets', File.dirname(__FILE__)), css_route: 'css'
 
   route do |r|
     @app = "App2"
