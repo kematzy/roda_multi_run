d9f680f7f4ede72c4654b393b71db606b431207d
Kematzy <kematzy@gmail.com>
2015-05-04 14:55:28 +0800

    App: updated for new apps/app* structure

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
-require "./app1/current/app1"
-require "./app2/current/app2"
+%w(app1 app2 app3).each { |a| require "./apps/#{a}/current/#{a}" }
 
 class App < Roda
+  opts[:root] = File.expand_path(File.dirname(__FILE__))
+  opts[:add_script_name] = true
+  
+  # make global images available
+  plugin :static, ['/images']
+  
   plugin :multi_run
   plugin :render
+  
 
-  run "foo", App1
-  run "bar", App2
+  run "app1", App1
+  run "app2", App2
+  run "app3", App3
 
   route do |r|
     @app = "Root"
@@ -15,6 +22,14 @@ class App < Roda
     r.root do
       view :index
     end
+    
+    r.is 'blog' do
+      r.get do
+        view :blog
+        # "Welcome to my non-existent blog!"
+      end
+    end
+    
   end
   freeze
 end
