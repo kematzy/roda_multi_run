4b2426c09cd24f1431c1f5d4ddf9ed6d581af0c1
Kematzy <kematzy@gmail.com>
2015-06-23 20:33:26 +0800

    App3 improvements

--- apps/app3/current/app3.rb
+++ apps/app3/current/app3.rb
@@ -6,7 +6,8 @@ class App3 < Roda
   opts[:add_script_name] = true
   
   plugin :render
-  plugin :static, ["/images"]
+  # plugin :static, ["/images"], :root => File.expand_path('./public', __FILE__)
+  plugin :static, ["/images"], :root => _fep('app/public')
   plugin :assets, css: 'app.css', path: _fep('app/assets')
   
   route do |r|
@@ -20,5 +21,29 @@ class App3 < Roda
   end
   
   require_relative 'app/helpers/app3-helpers'
+  
+  def image(fname)
+    uri(fname, true)
+  end
+  
+  def uri(addr = nil, add_script_name = true)
+    addr = addr.to_s if addr
+    return addr if addr =~ /\A[A-z][A-z0-9\+\.\-]*:/
+    uri = []
+    # uri = if absolute
+    #   h = if @env.has_key?(HTTP_X_FORWARDED_HOST) || port != (ssl? ? 443 : 80)
+    #     host_with_port
+    #   else
+    #     host
+    #   end
+    #   ["http#{'s' if ssl?}://#{h}"]
+    # else
+    #   [EMPTY_STRING]
+    # end
+    uri << request.script_name.to_s if add_script_name
+    uri << (addr || request.path_info)
+    File.join(uri)
+  end
+  
   
 end
